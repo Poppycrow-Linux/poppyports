b1d25f2a63b1a38f114cde121a135d1a7937c211
Valera <108476413+ValeraDX@users.noreply.github.com>
2026-07-19 13:24:37 +0300

    Add a sha256sum

--- main/linux-headers/recipe.py
+++ main/linux-headers/recipe.py
@@ -7,6 +7,7 @@ arch = "all"
 license = "GPL"
 
 sources = ["https://cdn.kernel.org/pub/linux/kernel/v7.x/linux-7.1.4.tar.xz"]
+sha256sum = ["1c63922a119675d38e3ae0f8f6ee07f15c41a786ab9ed66563749bb8c9a08e2e"]
 depends = []
 
 
