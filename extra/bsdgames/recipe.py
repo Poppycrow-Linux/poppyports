445bee2039e423497e8edc149010e40f50ae2607
cachewave <88933951+cachewave@users.noreply.github.com>
2026-07-20 16:36:20 +0300

    broken bsdgames recipe

---
+++ extra/bsdgames/recipe.py
@@ -0,0 +1,20 @@
+recipever = 0
+pkgname = "bsdgames"
+pkgver = "3.2.0"
+pkgrel = 0
+pkgdesc = "BSD Games"
+url = "https://github.com/thesamesam/bsd-games"
+arch = "x86_64"
+license = "BSD"
+
+sources = [f"https://github.com/thesamesam/bsd-games/archive/refs/tags/v3.2.tar.gz"]
+depends = ["ncurses"]
+
+def build(c):
+  c.SRCDIR = c.SRCDIR + f"/bsd-games-3.2" # TODO fix this is because tar files have a top level name
+  c.sh("patch", "-p1", "-i", c.PORTDIR + "/fixup.patch")
+  c.sh("./configure",f"--prefix={c.PKGDIR}/usr",f"--localstatedir={c.PKGDIR}/var/lib")
+  c.sh("make")
+
+def install(c):
+  c.sh("make", "install")
