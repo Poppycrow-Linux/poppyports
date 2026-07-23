24020d147a85a2f3346ce34df75f6c6e6bc4da4a
Ethan <emholt1@proton.me>
2026-07-22 21:55:26 -0400

    first round of sbus

--- main/bash/recipe.py
+++ main/bash/recipe.py
@@ -6,7 +6,7 @@ pkgdesc = "Bash is the GNU Project's shell—the Bourne Again SHell. This is an 
 url = "https://www.gnu.org/software/bash/"
 arch = "x86_64"
 license = "GPL v3 or later"
-
+sbu = 0.9
 
 sources = [f"https://ftp.gnu.org/gnu/{pkgname}/{pkgname}-{pkgver}.tar.gz"]
 sha256sum = ["0d5cd86965f869a26cf64f4b71be7b96f90a3ba8b3d74e27e8e9d9d5550f31ba"]
--- main/ncurses/recipe.py
+++ main/ncurses/recipe.py
@@ -7,7 +7,7 @@ url = "https://invisible-island.net/ncurses/"
 arch = "x86_64"
 license = "MIT/X11"
 maintainer = "cachewave"
-
+sbu = 1.0
 
 sources = [f"https://ftp.gnu.org/gnu/ncurses/{pkgname}-{pkgver}.tar.gz"]
 sha256sum = ["355b4cbbed880b0381a04c46617b7656e362585d52e9cf84a67e2009b749ff11"]
