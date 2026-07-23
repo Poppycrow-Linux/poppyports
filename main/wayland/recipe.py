f91eff0a9a59094a4d545e7a3bba3dc84f3057ae
samxyz30 <samxyz30@noreply.codeberg.org>
2026-07-23 23:13:06 +0200

    disable documentation generation in wayland package
    removes the requirement for pydot, doxygen and other useless shit

--- main/wayland/recipe.py
+++ main/wayland/recipe.py
@@ -14,7 +14,8 @@ depends = []
 
 def build(c):
   c.SRCDIR = c.SRCDIR + "/wayland-1.26.0" # TODO fix this is because tar files have a top level name
-  c.sh("meson","build/",f"--prefix={c.PKGDIR}")
+  # https://stackoverflow.com/questions/70191105/got-an-error-while-building-wayland-error-program-dot-not-found
+  c.sh("meson","build/",f"--prefix={c.PKGDIR}", "-Ddocumentation=false")
 
 def install(c):
   c.sh("ninja", "-C", "build/", "install")
