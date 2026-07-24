d3751de3ff8cb9631b453ab3849dab1870918fca
Ethan <emholt1@proton.me>
2026-07-24 04:40:28 -0400

    Port OpenSSL

---
+++ main/openssl/recipe.py
@@ -0,0 +1,18 @@
+pkgname = "openssl"
+pkgver = "4.0.1"
+pkgrel = 0
+pkgdesc = "Library for various kinds of cryptography"
+url = "openssl-library.org"
+arch = "x86_64"
+license = "apache-2.0"
+
+sources = [f"https://github.com/openssl/openssl/releases/download/openssl-{pkgver}/openssl-{pkgver}.tar.gz"]
+depends = []
+
+def build(c):
+  c.SRCDIR += f"/openssl-{pkgver}"
+  c.sh("./Configure", f"--prefix={c.PKGDIR}/usr")
+  c.sh("make", f"-j{c.NPROC}")
+
+def install(c):
+  c.sh("make", "install")
