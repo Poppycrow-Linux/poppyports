4edac09170f8f461da0b5d9fb1c976c9dbfd0d59
ValeraDX <ValeraDXX@gmail.com>
2026-07-25 16:53:44 +0300

    Add GCC recipe

---
+++ main/gcc/recipe.py
@@ -0,0 +1,36 @@
+recipever = 0
+pkgname = "gcc"
+pkgver = "16.1.0"
+pkgrel = 0
+pkgdesc = "GNU Compiler Collection"
+url = "https://gcc.gnu.org/"
+arch = "x86_64"
+license = "GPL v3"
+
+sbu = 18.0
+
+sources = [f"https://ftpmirror.gnu.org/gcc/gcc-{pkgver}/gcc-{pkgver}.tar.xz"]
+depends = ["gmp", "mpfr", "mpc", "isl", "zlib"]
+
+def build(c):
+  c.SRCDIR = c.SRCDIR + f"/{pkgname}-{pkgver}"
+  c.env["CC"] = "gcc"
+  c.env["CXX"] = "g++"
+
+  c.sh("mkdir", "-p", c.SRCDIR + "/build")
+  c.SRCDIR = c.SRCDIR + "/build"
+
+  c.sh("../configure",
+       "--prefix=/usr",
+       "--disable-multilib",
+       "--with-system-zlib",
+       "--enable-default-pie",
+       "--enable-default-ssp",
+       "--enable-host-pie",
+       "--disable-fixincludes",
+       "--enable-languages=c,c++")
+
+  c.sh("make", f"-j{c.NPROC}")
+
+def install(c):
+  c.sh("make", "install", f"DESTDIR={c.PKGDIR}")
