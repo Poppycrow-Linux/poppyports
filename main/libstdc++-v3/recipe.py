469a0d73f832a07527583fa78998e21b76ce1df2
samxyz30 <samxyz30@noreply.codeberg.org>
2026-07-27 10:06:41 +0000

    add libstdc++ v3 from gcc 16
    this should compile i dont know i copied lfs guide

---
+++ main/libstdc++-v3/recipe.py
@@ -0,0 +1,31 @@
+recipever = 0
+pkgname = "libstdc++-v3"
+pkgver = "16.1.0"
+pkgrel = 0
+pkgdesc = "libstdc++ V3, part of GCC sources"
+url = "https://gcc.gnu.org/"
+arch = "x86_64"
+license = "GPL v3"
+
+sbu = 18.0
+
+sources = [f"https://ftpmirror.gnu.org/gcc/gcc-{pkgver}/gcc-{pkgver}.tar.xz"]
+depends = []
+
+def build(c):
+  c.SRCDIR = c.SRCDIR + f"/gcc-{pkgver}"
+
+  c.sh("mkdir", "-p", c.SRCDIR + "/build")
+  c.SRCDIR = c.SRCDIR + "/build"
+
+  c.sh("../libstdc++-v3/configure",
+       "--prefix=/usr",
+       "--disable-multilib",
+       "--disable-nls",
+       "--disable-fixincludes",
+       "--disable-libstdcxx-pch")
+
+  c.sh("make", f"-j{c.NPROC}")
+
+def install(c):
+  c.sh("make", "install", f"DESTDIR={c.PKGDIR}")
