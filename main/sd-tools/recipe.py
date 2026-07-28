9f9a1b7526aaabf71c4983fb2cd08d05574403db
ValeraDX <ValeraDXX@gmail.com>
2026-07-28 21:24:59 +0300

    Add sd-tools
    I HATE MESON!!!

---
+++ main/sd-tools/recipe.py
@@ -0,0 +1,25 @@
+recipever = 0
+pkgname = "sd-tools"
+pkgver = "0.99.0"
+pkgrel = 0
+pkgdesc = "A collection of tools forked from systemd (tmpfiles and sysusers)"
+url = "https://github.com/chimera-linux/sd-tools"
+arch = "x86_64"
+license = "LGPL 2.1"
+
+#sources = [f"{url}/archive/refs/tags/v{pkgver}.tar.gz"]
+sources = ["https://github.com/chimera-linux/sd-tools/archive/refs/heads/master.tar.gz"] # master will do for now
+depends = []
+
+def build(c):
+  c.SRCDIR = c.SRCDIR + f"/{pkgname}-master" # TODO fix this is because tar files have a top level name
+  c.sh("meson","build/")
+  c.sh("ninja","all", cwd = f"{c.SRCDIR}/build")
+
+# hastag fast
+def install(c):
+  c.sh("mkdir","-p",f"{c.PKGDIR}/bin")
+  for i in ["sysusers", "tmpfiles"]:
+    c.sh(f"mkdir -p {c.PKGDIR}/usr/bin/{i}")
+    c.cp(f"{c.SRCDIR}/build/src/{i}/sd-{i}",f"{c.PKGDIR}/usr/bin/{i}")
+    c.lnk(f"{c.PKGDIR}/usr/bin/systemd-{i}", f"{c.PKGDIR}/usr/bin/{i}")
