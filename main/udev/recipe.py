13e49fcb5f45c0497693cd55cc080be485061e49
ValeraDX <ValeraDXX@gmail.com>
2026-07-31 20:46:05 +0300

    Repackage Udev according to LFS
    udev tricks they won't teach you at school

--- main/udev/recipe.py
+++ main/udev/recipe.py
@@ -2,7 +2,7 @@ recipever = 0
 pkgname = "udev"
 pkgver = "261.1"
 pkgrel = 0
-pkgdesc = "Udev from systemd without systemd"
+pkgdesc = "Udev extracted from systemd"
 url = "https://github.com/systemd/systemd"
 arch = "x86_64"
 license = "LGPL-2.1-or-later"
@@ -15,57 +15,80 @@ def build(c):
     c.env["CC"] = "gcc"
     c.env["CXX"] = "g++"
 
-    #bullshitted args that work somehow 
-    c.sh("meson", "setup", "build",
+    # remove the groups because lfs told us so
+    c.sh("sed", "-i",
+         "-e", "s/GROUP=\"render\"/GROUP=\"video\"/",
+         "-e", "s/GROUP=\"sgx\", //",
+         "rules.d/50-udev-default.rules.in")
+
+    # 2. more systemd bullshit
+    c.sh("sed", "-i", "/systemd-sysctl/s/^/#/", "rules.d/99-systemd.rules.in")
+
+    # 3. we don't need systemd vconsole!! fuck off!!!'
+    c.sh("rm", "-f", "rules.d/90-vconsole.rules")
+
+    meson_args = [
         "--prefix=/usr",
+        "--sysconfdir=/etc",
         "--buildtype=release",
         "-Dmode=release",
-        "-Dlink-udev-shared=false",
+        "-Ddev-kvm-mode=0660",        # LFS security override for kvm group
+        "-Dlink-udev-shared=false",   # We don't need a systemd shared libary!! fuck off!'
         "-Dstandalone-binaries=true",
-        "-Dbootloader=false",
+        "-Dsysusers=false",
+        "-Dtmpfiles=false",
+        "-Dhwdb=true",
+        # Disable all other systemd components. some options have disabled because otherwise it goes # DEPRECATION!!! false was replaced by disabled for no reason fuck you.
+        "-Dbootloader=disabled",
         "-Dlogind=false",
         "-Dnetworkd=false",
         "-Dtimesyncd=false",
         "-Dmachined=false",
-        "-Dimportd=false",
+        "-Dimportd=disabled",
         "-Dhostnamed=false",
         "-Dtimedated=false",
         "-Dlocaled=false",
         "-Dcoredump=false",
         "-Dinitrd=false",
         "-Dhibernate=false",
-        "-Dpam=false",
-        "-Dselinux=false",
-        "-Dpolkit=false",
+        "-Dpam=disabled",
+        "-Dselinux=disabled",
+        "-Dpolkit=disabled",
         "-Dbacklight=false",
         "-Drfkill=false",
-        "-Dpasswdqc=false",
+        "-Dpasswdqc=disabled",
         "-Dvconsole=false",
         "-Dquotacheck=false",
         "-Dldconfig=false",
-        "-Dman=false"
-        )
-        
-    c.sh("ninja", "-C", "build", f"-j{c.NPROC}")
+        "-Dman=disabled"
+    ]
+
+    c.sh("meson", "setup", "build", *meson_args)
+
+    c.sh("ninja", "-C", "build", "udevadm", "systemd-hwdb", f"-j{c.NPROC}")
+
 
 def install(c):
-    #big big ugly cleanu p
-    c.env["DESTDIR"] = c.PKGDIR
+    stage_dir = f"{c.SRCDIR}/_install_stage"
+    c.env["DESTDIR"] = stage_dir
 
+    # quantum we do not dump shit in tmp we do this in srcdir so it gets removed at some point ok????
     c.sh("ninja", "-C", "build", "install")
-    c.sh("mkdir", "-p", "/tmp/udev-keep/usr/bin", "/tmp/udev-keep/usr/lib/pkgconfig", "/tmp/udev-keep/usr/include")
 
-    c.sh("cp", "-a", c.PKGDIR + "/usr/bin/udevadm", "/tmp/udev-keep/usr/bin/", shell=True)
-    c.sh("cp", "-a", c.PKGDIR + "/usr/lib/libudev.so*", "/tmp/udev-keep/usr/lib/", shell=True)
-    c.sh("cp", "-a", c.PKGDIR + "/usr/include/libudev.h", "/tmp/udev-keep/usr/include/", shell=True)
-    c.sh("cp", "-a", c.PKGDIR + "/usr/lib/pkgconfig/libudev.pc", "/tmp/udev-keep/usr/lib/pkgconfig", shell=True)
+    c.sh("mkdir", "-p",
+         f"{c.PKGDIR}/usr/bin",
+         f"{c.PKGDIR}/usr/lib/pkgconfig",
+         f"{c.PKGDIR}/usr/include"
+    )
 
-    if c.sh("test", "-d", c.PKGDIR + "/usr/lib/udev"):
-        c.sh("cp", "-a", c.PKGDIR + "/usr/lib/udev", "/tmp/udev-keep/usr/lib/")
-    if c.sh("test", "-d", c.PKGDIR + "/etc/udev"):
-        c.sh("cp", "-a", c.PKGDIR + "/etc/udev", "/tmp/udev-keep/etc")
+    # WE LOVE CHERRY PICKING!!!!
+    c.sh(f"cp -a {stage_dir}/usr/bin/udevadm {c.PKGDIR}/usr/bin/", shell=True)
+    c.sh(f"cp -a {stage_dir}/usr/bin/systemd-hwdb {c.PKGDIR}/usr/bin/ || true", shell=True) # hwdb binary
 
-    c.sh("rm", "-rf", c.PKGDIR + "/*")
+    c.sh(f"cp -a {stage_dir}/usr/lib/libudev.so* {c.PKGDIR}/usr/lib/", shell=True)
+    c.sh(f"cp -a {stage_dir}/usr/include/libudev.h {c.PKGDIR}/usr/include/", shell=True)
+    c.sh(f"cp -a {stage_dir}/usr/lib/pkgconfig/libudev.pc {c.PKGDIR}/usr/lib/pkgconfig/", shell=True)
 
-    c.sh("cp", "-a", "/tmp/udev-keep/*", c.PKGDIR + "/", shell=True)
-    c.sh("rm", "-rf", "/tmp/udev-keep")
+    # udev rule number 1: udev has no rules. actually wait it does, let's copy them over.'
+    c.sh(f"[ -d {stage_dir}/usr/lib/udev ] && cp -a {stage_dir}/usr/lib/udev {c.PKGDIR}/usr/lib/ || true", shell=True)
+    c.sh(f"[ -d {stage_dir}/etc/udev ] && cp -a {stage_dir}/etc/udev {c.PKGDIR}/etc/ || true", shell=True)
