2ae9efd24fe8d69e2264972ce42f6e6da2d32bbb
ValeraDX <ValeraDXX@gmail.com>
2026-08-06 22:16:41 +0300

    first udev draft

--- main/udev/recipe.py
+++ main/udev/recipe.py
@@ -15,80 +15,68 @@ def build(c):
     c.env["CC"] = "gcc"
     c.env["CXX"] = "g++"
 
-    # remove the groups because lfs told us so
-    c.sh("sed", "-i",
-         "-e", "s/GROUP=\"render\"/GROUP=\"video\"/",
+    c.sh("sed", "-e", "s/GROUP=\"render\"/GROUP=\"video\"/",
          "-e", "s/GROUP=\"sgx\", //",
-         "rules.d/50-udev-default.rules.in")
+         "-i", f"{c.SRCDIR}/rules.d/50-udev-default.rules.in")
+    c.sh("sed", "-i", "/systemd-sysctl/s/^/#/",
+         f"{c.SRCDIR}/rules.d/99-systemd.rules.in")
+    c.sh("sed", "-e", "/NETWORK_DIRS/s/systemd/udev/",
+         "-i", f"{c.SRCDIR}/src/libsystemd/sd-network/network-util.h")
 
-    # 2. more systemd bullshit
-    c.sh("sed", "-i", "/systemd-sysctl/s/^/#/", "rules.d/99-systemd.rules.in")
+    builddir = f"{c.SRCDIR}/build"
+    c.sh("mkdir", "-p", builddir)
 
-    # 3. we don't need systemd vconsole!! fuck off!!!'
-    c.sh("rm", "-f", "rules.d/90-vconsole.rules")
-
-    meson_args = [
+    c.sh(
+        "meson", "setup", builddir,
+        c.SRCDIR,
         "--prefix=/usr",
-        "--sysconfdir=/etc",
         "--buildtype=release",
         "-Dmode=release",
-        "-Ddev-kvm-mode=0660",        # LFS security override for kvm group
-        "-Dlink-udev-shared=false",   # We don't need a systemd shared libary!! fuck off!'
-        "-Dstandalone-binaries=true",
-        "-Dsysusers=false",
-        "-Dtmpfiles=false",
-        "-Dhwdb=true",
-        # Disable all other systemd components. some options have disabled because otherwise it goes # DEPRECATION!!! false was replaced by disabled for no reason fuck you.
-        "-Dbootloader=disabled",
+        "-Ddev-kvm-mode=0660",
+        "-Dlink-udev-shared=false",
         "-Dlogind=false",
-        "-Dnetworkd=false",
-        "-Dtimesyncd=false",
-        "-Dmachined=false",
-        "-Dimportd=disabled",
-        "-Dhostnamed=false",
-        "-Dtimedated=false",
-        "-Dlocaled=false",
-        "-Dcoredump=false",
-        "-Dinitrd=false",
-        "-Dhibernate=false",
-        "-Dpam=disabled",
-        "-Dselinux=disabled",
-        "-Dpolkit=disabled",
-        "-Dbacklight=false",
-        "-Drfkill=false",
-        "-Dpasswdqc=disabled",
         "-Dvconsole=false",
-        "-Dquotacheck=false",
-        "-Dldconfig=false",
-        "-Dman=disabled"
-    ]
+    )
 
-    c.sh("meson", "setup", "build", *meson_args)
+    c.sh(f"""bash (grep "'name' :" {c.SRCDIR}/src/udev/meson.build | awk '{{print $3}}' | tr -d ",'" | grep -v 'udevadm') &&
+    ninja udevadm systemd-hwdb $(ninja -n | grep -Eo '(src/(lib)?udev|rules.d|hwdb.d)/[^ ]*') $(realpath libudev.so --relative-to .) > {builddir}/udev_helpers.txt
+        """, shell = True, cwd=builddir)
 
-    c.sh("ninja", "-C", "build", "udevadm", "systemd-hwdb", f"-j{c.NPROC}")
+    c.sh(
+        "bash", "-lc",
+        f"""
+        cd {builddir} &&
+        udev_helpers=$(grep "'name' :" {c.SRCDIR}/src/udev/meson.build | awk '{{print $3}}' | tr -d ",'" | grep -v 'udevadm') &&
+        ninja udevadm systemd-hwdb $(ninja -n | grep -Eo '(src/(lib)?udev|rules.d|hwdb.d)/[^ ]*') $(realpath libudev.so --relative-to .) $udev_helpers
+        """, shell = True
+    )
+
 
 
 def install(c):
-    stage_dir = f"{c.SRCDIR}/_install_stage"
-    c.env["DESTDIR"] = stage_dir
+    builddir = f"{c.SRCDIR}/build"
+    c.sh("install", "-vm755", "-d", f"{c.PKGDIR}/usr/bin")
+    c.sh("install", "-vm755", "-d", f"{c.PKGDIR}/usr/sbin")
+    c.sh("install", "-vm755", "-d", f"{c.PKGDIR}/usr/lib/udev/hwdb.d")
+    c.sh("install", "-vm755", "-d", f"{c.PKGDIR}/usr/lib/udev/rules.d")
+    c.sh("install", "-vm755", "-d", f"{c.PKGDIR}/usr/lib/udev/network")
+    c.sh("install", "-vm755", "-d", f"{c.PKGDIR}/usr/lib/pkgconfig")
+    c.sh("install", "-vm755", "-d", f"{c.PKGDIR}/usr/share/pkgconfig")
+    c.sh("install", "-vm755", "-d", f"{c.PKGDIR}/usr/include")
+    c.sh("install", "-vm755", "-d", f"{c.PKGDIR}/usr/lib/udev")
+    c.sh("install", "-vm755", "-d", f"{c.PKGDIR}/etc/udev")
 
-    # quantum we do not dump shit in tmp we do this in srcdir so it gets removed at some point ok????
-    c.sh("ninja", "-C", "build", "install")
-
-    c.sh("mkdir", "-p",
-         f"{c.PKGDIR}/usr/bin",
-         f"{c.PKGDIR}/usr/lib/pkgconfig",
-         f"{c.PKGDIR}/usr/include"
-    )
-
-    # WE LOVE CHERRY PICKING!!!!
-    c.sh(f"cp -a {stage_dir}/usr/bin/udevadm {c.PKGDIR}/usr/bin/", shell=True)
-    c.sh(f"cp -a {stage_dir}/usr/bin/systemd-hwdb {c.PKGDIR}/usr/bin/ || true", shell=True) # hwdb binary
-
-    c.sh(f"cp -a {stage_dir}/usr/lib/libudev.so* {c.PKGDIR}/usr/lib/", shell=True)
-    c.sh(f"cp -a {stage_dir}/usr/include/libudev.h {c.PKGDIR}/usr/include/", shell=True)
-    c.sh(f"cp -a {stage_dir}/usr/lib/pkgconfig/libudev.pc {c.PKGDIR}/usr/lib/pkgconfig/", shell=True)
-
-    # udev rule number 1: udev has no rules. actually wait it does, let's copy them over.'
-    c.sh(f"[ -d {stage_dir}/usr/lib/udev ] && cp -a {stage_dir}/usr/lib/udev {c.PKGDIR}/usr/lib/ || true", shell=True)
-    c.sh(f"[ -d {stage_dir}/etc/udev ] && cp -a {stage_dir}/etc/udev {c.PKGDIR}/etc/ || true", shell=True)
+    c.sh("install", "-vm755", f"{builddir}/udevadm", f"{c.PKGDIR}/usr/bin/")
+    c.sh("install", "-vm755", f"{builddir}/systemd-hwdb", f"{c.PKGDIR}/usr/bin/udev-hwdb")
+    c.sh("ln", "-svfn", "../bin/udevadm", f"{c.PKGDIR}/usr/sbin/udevd")
+    c.sh("cp", "-av", f"{builddir}/libudev.so", f"{c.PKGDIR}/usr/lib/")
+    c.sh("cp", "-av", f"{builddir}/libudev.so.*", f"{c.PKGDIR}/usr/lib/", shell = True)
+    #c.sh("cp", "-av", f"{builddir}/libudev.so{,*[0-9]}", f"{c.PKGDIR}/usr/lib/")
+    c.sh("install", "-vm644", f"{c.SRCDIR}/src/libudev/libudev.h", f"{c.PKGDIR}/usr/include/", shell = True)
+    c.sh("install", "-vm644", f"{builddir}/src/libudev/*.pc", f"{c.PKGDIR}/usr/lib/pkgconfig/", shell = True)
+    c.sh("install", "-vm644", f"{builddir}/src/udev/*.pc", f"{c.PKGDIR}/usr/share/pkgconfig/", shell = True)
+    c.sh("install", "-vm644", f"{c.SRCDIR}/src/udev/udev.conf", f"{c.PKGDIR}/etc/udev/", shell = True)
+    c.sh("install", "-vm644", f"{c.SRCDIR}/rules.d/*", f"{c.PKGDIR}/usr/lib/udev/rules.d/", shell = True)
+    c.sh("install", "-vm644", f"{c.SRCDIR}/hwdb.d/*", f"{c.PKGDIR}/usr/lib/udev/hwdb.d/", shell = True)
+    c.sh("install", "-vm755", "$udev_helpers", f"{c.PKGDIR}/usr/lib/udev", shell = True)
+    c.sh("install", "-vm644", f"{c.SRCDIR}/network/99-default.link", f"{c.PKGDIR}/usr/lib/udev/network")
