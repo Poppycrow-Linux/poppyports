10403225a9095d0f91f53116c9210ea3a7e97e84
ValeraDX <ValeraDXX@gmail.com>
2026-08-12 17:01:37 +0300

    Add libcap-ng recipe

---
+++ recipes/main/libcap-ng/recipe.py
@@ -0,0 +1,25 @@
+recipever = 0
+pkgname = "libcap-ng"
+pkgver = "0.9.3"
+pkgrel = 0
+pkgdesc = "The libcap-ng library should make programming with POSIX capabilities easier."
+url = "https://github.com/stevegrubb/libcap-ng"
+arch = "x86_64"
+license = "GPL-2.0-only"
+
+sbu = 0.1
+
+
+
+sources = [f"{url}/archive/refs/tags/v{pkgver}.tar.gz"]
+depends = []
+makedepends = ["make", "attr-devel", "linux-headers", "pkgconf", "perl"]
+
+def build(c):
+  c.SRCDIR = c.SRCDIR + f"/{pkgname}-{pkgver}"
+  c.sh("./autogen.sh")
+  c.sh("./configure")
+  c.sh("make")
+
+def install(c):
+  c.sh("make", "install", f"DESTDIR={c.PKGDIR}")
