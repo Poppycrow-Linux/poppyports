9e1533a7097fee456fb387848423b96624e96ecf
cachewave <cachewave@noreply.codeberg.org>
2026-08-18 16:12:54 +0300

    add mainline kernel

---
+++ recipes/main/linux-mainline/recipe.py
@@ -0,0 +1,22 @@
+recipever = 0
+pkgname = "linux-mainline"
+pkgver = "7.2"
+pkgrel = 0
+pkgdesc = "Linux Kernel (mainline)"
+url = "https://kernel.org/"
+arch = "x86_64"
+license = "GPL v2"
+
+sources = [f"https://cdn.kernel.org/pub/linux/kernel/v7.x/linux-{pkgver}.tar.xz"]
+depends = []
+
+def build(c):
+  c.SRCDIR = c.SRCDIR + f"/linux-{pkgver}" # TODO fix this is because tar files have a top level name
+  c.sh("make", "defconfig")
+  c.cp(f"{c.PORTDIR}/.config",f"{c.SRCDIR}/.config")
+  c.sh("make",f"-j{c.NPROC}","LLVM=1")
+  #c.sh("make","modules_install",f"-j{c.NPROC}")
+
+def install(c):
+  c.sh("mkdir", "-p", f"{c.PKGDIR}/boot")
+  c.sh("cp", f"{c.SRCDIR}/arch/x86/boot/bzImage",f"{c.PKGDIR}/boot/bzImage")
