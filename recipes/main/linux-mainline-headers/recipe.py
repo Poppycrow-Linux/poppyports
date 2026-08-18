ec75015c4315710afc7b10184740f7311af3dae7
cachewave <cachewave@noreply.codeberg.org>
2026-08-18 16:15:50 +0300

    add mainline headers

---
+++ recipes/main/linux-mainline-headers/recipe.py
@@ -0,0 +1,21 @@
+recipever = 0
+pkgname = "linux-mainline-headers"
+pkgver = "7.2"
+pkgrel = 0
+pkgdesc = "Headers for the mainline Linux kernel"
+url = "https://www.kernel.org/"
+arch = "all"
+license = "GPL"
+
+sources = [f"https://cdn.kernel.org/pub/linux/kernel/v7.x/linux-{pkgver}.tar.xz"]
+#sha256sum = ["1c63922a119675d38e3ae0f8f6ee07f15c41a786ab9ed66563749bb8c9a08e2e"]
+depends = []
+
+
+def build(c):
+  c.SRCDIR = c.SRCDIR + f"/linux-{pkgver}"  # TODO fix this is because tar files have a top level name
+  c.sh("make", "mrproper") # TODO Move to a prepare() !!!
+
+def install(c):
+  c.sh("make", "headers_install", f"INSTALL_HDR_PATH={c.PKGDIR}/usr")
+
