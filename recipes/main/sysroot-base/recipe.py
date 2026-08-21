870422ad9d127b58f970f9dc9d0ae381b5b69143
ValeraDX <ValeraDXX@gmail.com>
2026-08-21 16:53:26 +0300

    add package: sysroot-base

---
+++ recipes/main/sysroot-base/recipe.py
@@ -0,0 +1,38 @@
+pkgname = "sysroot-base"
+pkgver = "0.0.1"
+pkgrel = 0
+pkgdesc = "Poppycrow sysroot system package"
+url = "https://www.poppycrow.org/"
+arch = "x86_64"
+license = "BSD3"
+sources = []
+
+depends = [
+    "main/bash",
+    "main/gcc", #libstdc++ is not included since it is a part of gcc
+    "main/chimerautils",
+    "main/ncurses",
+    "main/libedit",
+    "main/linux-pam",
+    "main/glibc",
+    "main/bzip2",
+    "main/pcre2",
+    "main/gdbm",
+    "main/readline",
+    "main/apk-tools",
+    "main/ncurses",
+    "main/zlib",
+    "main/xz",
+    "main/openssl",
+    "main/libcap",
+    "main/acl",
+    "main/libdinitctl",
+]
+
+
+def build(c):
+    pass
+
+
+def install(c):
+   c.cp(f"{c.PORTDIR}/overlay/.", c.PKGDIR)
