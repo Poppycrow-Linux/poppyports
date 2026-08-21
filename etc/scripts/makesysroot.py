e1291c5d08f3fd741fe3715435eedc56c7cb941b
ValeraDX <ValeraDXX@gmail.com>
2026-08-21 21:54:32 +0300

    add makesysroot script

---
+++ etc/scripts/makesysroot.py
@@ -0,0 +1,79 @@
+import argparse
+import os
+import shutil
+import subprocess
+
+parser = argparse.ArgumentParser(
+    prog="makesysroot",
+    # suggest_on_error=True, # this doesn't work on my python 3.13
+    description="Makes the rootfs which just so happens to be a viable sysroot",
+    epilog="See more @ https://codeberg.org/Poppycrow-Linux/poppyports",
+)
+
+parser.add_argument("-force", "-force-rebuild", "-fr", "-fresh", action="store_true", help="Deletes the rootfs before building.")
+
+args = parser.parse_args()
+force = args.force
+
+print("Reading sysroot-base dependencies")
+recipe = {}
+with open("recipes/main/sysroot-base/recipe.py", "r") as f:
+  exec(f.read(), recipe)
+
+depends = ["main/sysroot-base"] + recipe["depends"]
+print(depends)
+
+# set up rootfs
+"""
+/sbin links to /usr/sbin
+/usr/sbin links to /bin which then links to /usr/bin
+/bin links to /usr/bin
+/lib links to /usr/lib
+/lib32 links to /usr/lib32
+/lib64 links to /usr/lib64
+/dev
+/dev/pts
+/dev/shm
+/sys
+/sys/fs/cgroup
+/proc
+"""
+
+if os.path.exists("build/sysroot"):
+    if not(force):
+        print("sysroot already exists. delete it before running this")
+        exit(1)
+    else:
+        print("Force flag was passed, nuking the sysroot dir!")
+        shutil.rmtree("build/sysroot")
+
+
+
+subprocess.run(("mkdir","-p","build/sysroot/sys/fs/cgroup"))
+subprocess.run(("mkdir","-p","build/sysroot/dev/pts"))
+subprocess.run(("mkdir","-p","build/sysroot/dev/shm"))
+subprocess.run(("mkdir","-p","build/sysroot/proc"))
+subprocess.run(("mkdir","-p","build/sysroot/run"))
+subprocess.run(("mkdir","-p","build/sysroot/usr/bin"))
+subprocess.run(("mkdir","-p","build/sysroot/usr/lib"))
+subprocess.run(("ln", "-s", "usr/bin", "bin"), cwd= os.getcwd()+"/build/sysroot")
+subprocess.run(("ln", "-s", "usr/bin", "sbin"), cwd= os.getcwd()+"/build/sysroot")
+subprocess.run(("ln", "-s", "bin", "sbin"), cwd=os.getcwd()+"/build/sysroot/usr")
+subprocess.run(("ln","-s","usr/lib","lib"),cwd=os.getcwd()+"/build/sysroot")
+#subprocess.run(("ln","-s","usr/lib","lib32"),cwd=os.getcwd()+"/build/sysroot")
+subprocess.run(("ln","-s","usr/lib","lib64"),cwd=os.getcwd()+"/build/sysroot")
+subprocess.run(("ln","-s","lib","lib64"),cwd=os.getcwd()+"/build/sysroot/usr")
+#subprocess.run(("ln","-s","lib","lib32"),cwd=os.getcwd()+"/build/sysroot/usr")
+subprocess.run(("ln","-s",".","./x86_64-linux-gnu"),cwd=os.getcwd()+"/build/sysroot/lib") # ugly hack
+subprocess.run(("rsync","-r","-v","recipes/main/poppy-base/overlay/.","build/sysroot/"))
+
+
+for dep in depends:
+  print(f"building {dep}")
+  subprocess.run(["python3", "pbuild", f"{dep}", f"build/pkg/{dep}"])
+  print(f"Copying the contents of build/pkg/{dep}/pkgdir/ to build/sysroot/")
+  subprocess.run(["rsync", "-r", "-l", "-K", "-H", f"build/pkg/{dep}/pkgdir/.", "build/sysroot/"])
+
+# TODO: print(f"generating apkindex")
+#       do this in build/repo/APKINDEX.tar.gz
+#       oh also copy all the apks there.
