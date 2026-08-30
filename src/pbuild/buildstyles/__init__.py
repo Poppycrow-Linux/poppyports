e81b7fd45f205aabc430581f24b5f48b185fc249
ValeraDX <ValeraDXX@gmail.com>
2026-08-30 14:27:45 +0300

    register the new kernel and kernel-headers buildstyles

--- src/pbuild/buildstyles/__init__.py
+++ src/pbuild/buildstyles/__init__.py
@@ -1,7 +1,12 @@
+# this is where the buildstyles are registered. this is done to avoid importing everything
+# in pbuild, so anything that could need buildstyles can just import them
+
 # from .cmake import CMakeStyle
 from .gnu_configure import GnuConfigureStyle
 from .makefile import MakefileStyle
 from .meson import MesonStyle
+from .kernel import KernelStyle
+from .kernel_headers import KernelHeadersStyle
 
 
 BUILD_STYLES = {
@@ -9,6 +14,8 @@ BUILD_STYLES = {
   "gnu_configure": GnuConfigureStyle,
   "makefile": MakefileStyle,
   "meson": MesonStyle,
+  "kernel": KernelStyle,
+  "kernel_headers": KernelHeadersStyle,
 }
 
 
