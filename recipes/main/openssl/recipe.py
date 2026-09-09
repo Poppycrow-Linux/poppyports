f388c996d207713597e1c5d5b6ca2521f5b67fe2
ValeraDX <ValeraDXX@gmail.com>
2026-09-09 19:19:34 +0300

    update openssl recipe to new format

--- recipes/main/openssl/recipe.py
+++ recipes/main/openssl/recipe.py
@@ -3,16 +3,32 @@ pkgver = "3.5.7"
 pkgrel = 0
 pkgdesc = "Library for various kinds of cryptography"
 url = "openssl-library.org"
-arch = "x86_64"
-license = "apache-2.0"
+arch = ["x86_64", "aarch64"]
+license = "Apache-2.0"
 
 sources = [f"https://github.com/openssl/openssl/releases/download/openssl-{pkgver}/openssl-{pkgver}.tar.gz"]
 depends = ["libc"]
+hostmakedepends = ["pkgconf", "perl"]
+build_style = "gnu_configure"
+configure_command = "Configure"
+build_wrksrc = f"{pkgname}-{pkgver}"
+configure_args = ["--prefix=/usr", "shared", "--openssldir=/etc/ssl"]
 
-def build(c):
-  c.SRCDIR += f"/openssl-{pkgver}"
-  c.sh("./Configure", f"--prefix={c.PKGDIR}/usr")
-  c.sh("make", f"-j{c.NPROC}")
+add_cross_params = False
+make_check = False
 
-def install(c):
-  c.sh("make", "install")
+def prepare(c):
+  global configure_args
+  c.env["CROSS_COMPILE"] = "" # needed because openssl treats it as a prefix. the fuck???
+  if c.ARCH == "x86_64":
+    configure_args += ["enable-ec_nistp_64_gcc_128", "linux-x86_64"]
+  elif c.ARCH in ("aarch64", "ppc64le", "ppc64", "ppc"):
+    configure_args += [f"linux-{c.ARCH}"]
+  elif c.ARCH in ("riscv64", "loongarch64"):
+    configure_args += [f"linux64-{c.ARCH}"]
+  elif c.ARCH in ("armhf", "armv7"):
+    configure_args += ["linux-armv4"] # who the fuck calls armv7 armv4 lmao
+  else:
+    raise RuntimeError(f"Unknown CPU architecture: {c.ARCH}")
+  configure_args += c.CFLAGS.split()
+  configure_args += c.LDFLAGS.split()
